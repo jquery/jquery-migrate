4c5bf7a0d7e98f73fc738778b52090a5b91b5be2
Dave Methvin <dave.methvin@gmail.com>
2012-12-07 17:24:05 -0500

    Only warn on buildFragment if there isn't one.

--- src/manipulation.js
+++ src/manipulation.js
@@ -1,18 +1,29 @@
 
+var oldSelf = jQuery.fn.andSelf || jQuery.fn.addBack,
+	oldFragment = jQuery.buildFragment;
+
+jQuery.fn.andSelf = function() {
+	compatWarn("jQuery.fn.andSelf() replaced by jQuery.fn.addBack()");
+	return oldSelf.apply( this, arguments );
+};
+
 jQuery.buildFragment = function( args, context, scripts ) {
 	var fragment;
 
-	compatWarn("jQuery.buildFragment() is deprecated");
+	if ( !oldFragment ) {
+		// Set context from what may come in as undefined or a jQuery collection or a node
+		// Updated to fix #12266 where accessing context[0] could throw an exception in IE9/10 &
+		// also doubles as fix for #8950 where plain objects caused createDocumentFragment exception
+		context = context || document;
+		context = !context.nodeType && context[0] || context;
+		context = context.ownerDocument || context;
 
-	// Set context from what may come in as undefined or a jQuery collection or a node
-	// Updated to fix #12266 where accessing context[0] could throw an exception in IE9/10 &
-	// also doubles as fix for #8950 where plain objects caused createDocumentFragment exception
-	context = context || document;
-	context = !context.nodeType && context[0] || context;
-	context = context.ownerDocument || context;
+		fragment = context.createDocumentFragment();
+		jQuery.clean( args, context, fragment, scripts );
 
-	fragment = context.createDocumentFragment();
-	jQuery.clean( args, context, fragment, scripts );
-
-	return fragment;
+		compatWarn("jQuery.buildFragment() is deprecated");
+		return { fragment: fragment, cacheable: false };
+	}
+	// Don't warn if we are in a version where buildFragment is used internally
+	return oldFragment.apply( this, arguments );
 };
--- test/manipulation.js
+++ test/manipulation.js
@@ -1,26 +1,34 @@
 
 module( "manipulation", { setup: jQuery.compatReset });
 
+test( "andSelf", function() {
+	expect( 2 );
+
+	var warnLength = jQuery.compatWarnings.length,
+		$test = jQuery("<div>Beautiful<p>World</p></div>").find("p").andSelf();
+	
+	equal( $test.length, 2, "correct element count" );
+	equal( warnLength + 1, jQuery.compatWarnings.length, "jQuery.fn.andSelf warned" );
+});
+
 test( "buildFragment", function() {
-	expect( 7 );
+	expect( 6 );
 
-	var frag, div, html,
-		warnLength = jQuery.compatWarnings.length;
+	var frag, div, html;
 
-	frag = jQuery.buildFragment( [], document );
+	frag = jQuery.buildFragment( [], [ document ] ).fragment;
 	equal( frag.childNodes.length, 0, "empty frag is empty" );
 	
-	frag = jQuery.buildFragment( [ document.createElement( "b" ) ], null );
+	frag = jQuery.buildFragment( [ document.createElement( "b" ) ], [ document ] ).fragment;
 	equal( frag.childNodes.length, 1, "frag from DOM element" );
 	equal( frag.firstChild.nodeName.toLowerCase(), "b", "frag node is correct" );
 
 	html = "<p>sue<em>bawls</em></p><div>cheese<em>balls</em></div>";
-	frag = jQuery.buildFragment( [ html ] );
+	frag = jQuery.buildFragment( [ html ], [ document ] ).fragment;
 	equal( frag.childNodes.length, 2, "frag from HTML" );
 	equal( frag.lastChild.nodeName.toLowerCase(), "div", "frag last node is correct" );
 	div = document.createElement( "div" );
 	div.appendChild( frag );
 	equal( div.innerHTML.toLowerCase().replace( /[\s\n]+/, "" ), html, "frag html" );
-	
-	equal( warnLength + 1, jQuery.compatWarnings.length, "jQuery.buildFragment warned" );
+	// Not checking for a warning, it won't occur when e.g. 1.7.2 is used
 });
