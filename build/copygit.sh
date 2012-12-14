37f4b3112fe4ccb297d897de34ece529838ecbcc
Dave Methvin <dave.methvin@gmail.com>
2012-12-14 17:40:50 -0500

    Line endings!

--- build/copygit.sh
+++ build/copygit.sh
@@ -4,3 +4,4 @@ curl http://code.origin.jquery.com/jquery-compat-git.js?reload
 
 scp ../dist/jquery-compat.min.js jqadmin@code.origin.jquery.com:/var/www/html/code.jquery.com/jquery-compat-git.min.js
 curl http://code.origin.jquery.com/jquery-compat-git.min.js?reload
+
