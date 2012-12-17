9bb9d3d55ac4ee594712106fefd2896e0b8edcf7
Dave Methvin <dave.methvin@gmail.com>
2012-12-17 13:33:25 -0500

    Ensure jQuery.noConflict() runs before the plugin loads.

--- test/testinit.js
+++ test/testinit.js
@@ -1,29 +1,26 @@
-(function( name ) {
+/*
+ * Load a version of a file based on URL parameters.
+ *
+ *	dev		Uncompressed development version in the project /dist dir
+ *	min		Minified version in the project /dist dir
+ *	VER		Version from code.jquery.com, e.g.: git, 1.8.2.min or 1.7rc1
+ *	else	Full or relative path to be used for script src
+ */
+function loadVersion( project, urlTag, defaultVersion ) {
+	var file,
+		matcher = new RegExp( "\\b" + urlTag + "=([^&]+)" ),
+		version = ( matcher.exec( document.location.search ) || {} )[1] || defaultVersion;
 
-	/*
-	 * Load a version of jQuery and the plugin based on URL parameters.
-	 *
-	 *	dev		Uncompressed development version in the project /dist dir
-	 *	min		Minified version in the project /dist dir
-	 *	VER		Version from code.jquery.com, e.g.: git, 1.8.2.min or 1.7rc1
-	 *	else	Full or relative path to be used for script src
-	 */
-	var jquery = ( /\bjquery=([^&]+)/.exec( document.location.search ) || {} )[1] || "git",
-		plugin = ( /\bplugin=([^&]+)/.exec( document.location.search ) || {} )[1] || "dev";
-
-	function include( project, version ) {
-		var file = version;
-		if ( version === "dev" ) {
-			file = "../dist/" + project + ".js";
-		} else if ( version === "min" ) {
-			file = "../dist/" + project + ".min.js";
-		} else if ( /^[\w\.]+$/.test( version ) ) {
-			file = "http://code.jquery.com/" + project + "-" + version + ".js";
-		}
-		// Prevents a jshint warning about eval-like behavior of document.write
-		document["write"]( "<script src='" + file + "'></script>" );
+	if ( version === "dev" ) {
+		file = "../dist/" + project + ".js";
+	} else if ( version === "min" ) {
+		file = "../dist/" + project + ".min.js";
+	} else if ( /^[\w\.]+$/.test( version ) ) {
+		file = "http://code.jquery.com/" + project + "-" + version + ".js";
+	} else {
+		file = version;
 	}
-	include( "jquery", jquery );
-	include( name, plugin );
 
-}("jquery-migrate"));
+	// Prevents a jshint warning about eval-like behavior of document.write
+	document["write"]( "<script src='" + file + "'></script>" );
+}
