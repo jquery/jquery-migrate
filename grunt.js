722b984c99163b8b3453a69e2b6f20eccbaea54e
Dave Methvin <dave.methvin@gmail.com>
2012-12-16 22:45:58 -0500

    Update grunt for newer node.js

--- grunt.js
+++ grunt.js
@@ -1,5 +1,6 @@
 /*global module:false*/
 module.exports = function(grunt) {
+	"use strict";
 
 	function readOptionalJSON( filepath ) {
 		var data = {};
@@ -12,7 +13,7 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-    pkg: '<json:package.json>',
+    pkg: "<json:package.json>",
 	files: [
 		"src/intro.js",
 		"src/migrate.js",
@@ -24,27 +25,27 @@ module.exports = function(grunt) {
 		"src/outro.js"
 	],
     banners: {
-		full: '/*!\n * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-			' * <%= pkg.homepage %>\n' +
-			' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */',
-		tiny: '/*! <%= pkg.name %> <%= pkg.version %> - <%= pkg.homepage %> */'
+		full: "/*!\n * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
+			"<%= grunt.template.today('yyyy-mm-dd') %>\n" +
+			" * <%= pkg.homepage %>\n" +
+			" * Copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>;" +
+			" Licensed <%= _.pluck(pkg.licenses, 'type').join(', ') %>\n */",
+		tiny: "/*! <%= pkg.name %> <%= pkg.version %> - <%= pkg.homepage %> */"
     },
     concat: {
       dist: {
-        src: ['<banner:banners.full>', '<config:files>' ],
-        dest: 'dist/<%= pkg.name %>.js'
+        src: [ "<banner:banners.full>", "<config:files>" ],
+        dest: "dist/<%= pkg.name %>.js"
       }
     },
     min: {
       dist: {
-        src: ['<banner:banners.tiny>', '<config:files>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: [ "<banner:banners.tiny>", "<config:files>" ],
+        dest: "dist/<%= pkg.name %>.min.js"
       }
     },
     qunit: {
-      files: ['test/**/*.html']
+      files: [ "test/**/*.html" ]
     },
 	lint: {
 		dist: "dist/<% pkg.name %>.js",
@@ -52,8 +53,8 @@ module.exports = function(grunt) {
 		tests: "test/**/*.js"
 	},
     watch: {
-      files: '<config:lint.files>',
-      tasks: 'concat min lint'
+      files: "<config:lint.files>",
+      tasks: "concat min lint"
     },
     jshint: (function() {
 		function jshintrc( path ) {
@@ -61,8 +62,8 @@ module.exports = function(grunt) {
 		}
 		return {
 			grunt: jshintrc(),
-			dist: jshintrc( "src/" ),
-			tests: jshintrc( "test/" )
+			dist: jshintrc("src/"),
+			tests: jshintrc("test/")
 		};
 	}()),
 	uglify: {
@@ -78,6 +79,6 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', 'concat min lint qunit');
+  grunt.registerTask( "default", "concat min lint qunit" );
 
 };
