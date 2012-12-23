65535464db58549dd88a332c28d6d40a0a5d5ebc
Dave Methvin <dave.methvin@gmail.com>
2012-12-22 22:05:07 -0500

    Support/display the jQuery and plugin version choice. Close gh-3.
    This probably makes unwise assumptions about QUnit internals, but on the other hand it works. I have a QUnit patch that achieves the goal but can't get some QUnit tests to pass so this will do for now.

--- test/testinit.js
+++ test/testinit.js
@@ -1,26 +1,97 @@
-/*
- * Load a version of a file based on URL parameters.
- *
- *	dev		Uncompressed development version in the project /dist dir
- *	min		Minified version in the project /dist dir
- *	VER		Version from code.jquery.com, e.g.: git, 1.8.2.min or 1.7rc1
- *	else	Full or relative path to be used for script src
- */
-function loadVersion( project, urlTag, defaultVersion ) {
-	var file,
-		matcher = new RegExp( "\\b" + urlTag + "=([^&]+)" ),
-		version = ( matcher.exec( document.location.search ) || {} )[1] || defaultVersion;
 
-	if ( version === "dev" ) {
-		file = "../dist/" + project + ".js";
-	} else if ( version === "min" ) {
-		file = "../dist/" + project + ".min.js";
-	} else if ( /^[\w\.]+$/.test( version ) ) {
-		file = "http://code.jquery.com/" + project + "-" + version + ".js";
-	} else {
-		file = version;
+TestManager = {
+	/*
+	 * Load a version of a file based on URL parameters.
+	 *
+	 *	dev		Uncompressed development version in the project /dist dir
+	 *	min		Minified version in the project /dist dir
+	 *	VER		Version from code.jquery.com, e.g.: git, 1.8.2.min or 1.7rc1
+	 *	else	Full or relative path to be used for script src
+	 */
+	loadProject: function( projectName, defaultVersion ) {
+		var file,
+			urlTag = this.projects[ projectName ].urlTag,
+			matcher = new RegExp( "\\b" + urlTag + "=([^&]+)" ),
+			version = ( matcher.exec( document.location.search ) || {} )[1] || defaultVersion;
+
+		if ( version === "dev" ) {
+			file = "../../" + projectName + "/dist/" + projectName + ".js";
+		} else if ( version === "min" ) {
+			file = "../../" + projectName + "/dist/" + projectName + ".min.js";
+		} else if ( /^[\w\.]+$/.test( version ) ) {
+			file = "http://code.jquery.com/" + projectName + "-" + version + ".js";
+		} else {
+			file = version;
+		}
+		this.loaded.push({
+			projectName: projectName,
+			tag: version,
+			file: file
+		});
+
+		// Prevents a jshint warning about eval-like behavior of document.write
+		document["write"]( "<script src='" + file + "'></script>" );
+	},
+	/*
+	 *	Set the list of projects, including the project version choices.
+	 */
+	init: function( projects ) {
+		var self = this,
+			html = "";
+
+		this.projects = projects;
+		this.loaded = [];
+
+		function injectSelects() {
+			var i, c, project, tag, choices, toolbar, loaded, elem, selects;
+
+			for ( i = 0; i < self.loaded.length; i++ ) {
+				loaded = self.loaded[ i ];
+				project = projects[ loaded.projectName ];
+				tag = loaded.tag;
+				choices = project.choices.split(",");
+				if ( choices[ 0 ] !== tag ) {
+					choices.unshift( tag );
+				}
+
+				html += " " + project.urlTag + ":<select name='" + project.urlTag + "'>";
+				for ( c = 0; c < choices.length; c++ ) {
+					html += "<option value='" + choices[ c ] + "'" +
+						(c ? "" : " selected" ) + ">" + choices[ c ] + "</option>";
+				}
+				html += "</select>";
+			}
+
+			// Reach into QUnit's toolbar to add our selects;
+			// if this returns null then our assumptions have failed.
+			toolbar = document.getElementById("qunit-testrunner-toolbar");
+			toolbar.insertAdjacentHTML( "beforeend", html );
+
+			// Connect the selects so they reload the page with an updated URL
+			function updateUrl( e ) {
+				var arg = {},
+					target = e.target || event.srcElement;
+				arg[ target.name ] = target.value;
+				window.location = QUnit.url( arg );
+			}
+			selects = toolbar.getElementsByTagName("select");
+			for ( c = 0; c < selects.length; c++ ) {
+				elem = selects[ c ];
+				if ( elem.addEventListener ) {
+					elem.addEventListener( "change", updateUrl, false );
+				} else if ( elem.attachEvent ) {
+					elem.attachEvent( "onchange", updateUrl );
+				}
+			}
+		}
+
+		// Since we're adding a load handler after QUnit, the toolbar should be present
+		if ( window.addEventListener ) {
+			window.addEventListener( "load", injectSelects, false );
+		} else {
+			window.attachEvent( "onload", injectSelects );
+		}
 	}
+};
 
-	// Prevents a jshint warning about eval-like behavior of document.write
-	document["write"]( "<script src='" + file + "'></script>" );
-}
+
