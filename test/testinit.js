8e63aa7823396bfcc01474beef8ea5064c668df7
Dave Methvin <dave.methvin@gmail.com>
2013-01-30 14:38:43 -0500

    Do not assume project directory name.

--- test/testinit.js
+++ test/testinit.js
@@ -8,16 +8,17 @@ TestManager = {
 	 *	VER		Version from code.jquery.com, e.g.: git, 1.8.2.min or 1.7rc1
 	 *	else	Full or relative path to be used for script src
 	 */
-	loadProject: function( projectName, defaultVersion ) {
+	loadProject: function( projectName, defaultVersion, isSelf ) {
 		var file,
 			urlTag = this.projects[ projectName ].urlTag,
 			matcher = new RegExp( "\\b" + urlTag + "=([^&]+)" ),
+			projectRoot = isSelf ? ".." : "../../" + projectName,
 			version = ( matcher.exec( document.location.search ) || {} )[1] || defaultVersion;
 
 		if ( version === "dev" ) {
-			file = "../../" + projectName + "/dist/" + projectName + ".js";
+			file = projectRoot + "/dist/" + projectName + ".js";
 		} else if ( version === "min" ) {
-			file = "../../" + projectName + "/dist/" + projectName + ".min.js";
+			file = projectRoot + "/dist/" + projectName + ".min.js";
 		} else if ( /^[\w\.]+$/.test( version ) ) {
 			file = "http://code.jquery.com/" + projectName + "-" + version + ".js";
 		} else {
