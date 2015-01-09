683b9b3ae40e3fe9e83f3dd39d8e5e958975f3fa
Dave Methvin <dave.methvin@gmail.com>
2015-01-08 20:12:27 -0500

    Core: Fill in context and selector properties for .live() support
    Fixes #79
    
    Ref https://github.com/jquery/jquery/issues/1908

--- src/traversing.js
+++ src/traversing.js
@@ -1,6 +1,14 @@
-var oldSelf = jQuery.fn.andSelf || jQuery.fn.addBack;
+var oldSelf = jQuery.fn.andSelf || jQuery.fn.addBack,
+	oldFind = jQuery.fn.find;
 
 jQuery.fn.andSelf = function() {
 	migrateWarn("jQuery.fn.andSelf() replaced by jQuery.fn.addBack()");
 	return oldSelf.apply( this, arguments );
+};
+
+jQuery.fn.find = function( selector ) {
+	var ret = oldFind.apply( this, arguments );
+	ret.context = this.context;
+	ret.selector = this.selector ? this.selector + " " + selector : selector;
+	return ret;
 };
