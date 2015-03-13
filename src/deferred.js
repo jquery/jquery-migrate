32e1b7f620d39b623c3beff8a7221c7f5a5b445f
Dave Methvin <dave.methvin@gmail.com>
2015-03-13 13:42:37 -0400

    [squash]: Test and shim the deferred.promise().pipe method as well

--- src/deferred.js
+++ src/deferred.js
@@ -4,10 +4,18 @@ var oldDeferred = jQuery.Deferred;
 jQuery.Deferred = function() {
 	var deferred = oldDeferred.apply( this, arguments );
 
-	deferred.pipe = function() {
-		migrateWarn( "deferred.pipe() is deprecated" );
-		return deferred.then.apply( this, arguments );
-	};
-	
+	// Don't add this method if the current jQuery doesn't provide it
+	if ( deferred.pipe ) {
+
+		deferred.pipe = function() {
+			migrateWarn( "deferred.pipe() is deprecated" );
+			return deferred.then.apply( this, arguments );
+		};
+
+		deferred.promise().pipe = function() {
+			return deferred.pipe.apply( this, arguments );
+		};
+	}
+
 	return deferred;
 };
--- test/deferred.js
+++ test/deferred.js
@@ -2,10 +2,11 @@
 module("deferred");
 
 test( ".pipe()", function() {
-	expect( 4 );
+	expect( 8 );
 
 	var d = jQuery.Deferred().resolve( 1 );
 
+	// Deferred
 	expectNoWarning( "then", function() {
 		d.then(function( v ) {
 			equal( v, 1, "got correct value" );
@@ -16,6 +17,18 @@ test( ".pipe()", function() {
 			equal( v, 1, "got correct value" );
 		});
 	});
-		
+
+	// Deferred's promise object
+	d = d.promise();
+	expectNoWarning( "then", function() {
+		d.then(function( v ) {
+			equal( v, 1, "got correct value" );
+		});	});
+
+	expectWarning( "pipe", function() {
+		d.pipe(function( v ) {
+			equal( v, 1, "got correct value" );
+		});
+	});
 });
 
