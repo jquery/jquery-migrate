920afc6637645629e8cc22c9b76cd2fdc59e0d96
Dave Methvin <dave.methvin@gmail.com>
2015-04-28 09:58:06 -0400

    Deferred: Properly handle jQuery versions < 1.9.x
    Deferred was reimplemented using jQuery.Callbacks in 1.7 so this
    skips the shim if we're on 1.6.4 which is currently supported.
    Context behavior changed in 1.9 so don't make assertions on it.
    
    Ref #101

--- src/deferred.js
+++ src/deferred.js
@@ -1,63 +1,68 @@
 
-var oldDeferred = jQuery.Deferred,
-	tuples = [
-		// action, add listener, callbacks, .then handlers, final state
-		[ "resolve", "done", jQuery.Callbacks("once memory"),
-			jQuery.Callbacks("once memory"), "resolved" ],
-		[ "reject", "fail", jQuery.Callbacks("once memory"),
-			jQuery.Callbacks("once memory"), "rejected" ],
-		[ "notify", "progress", jQuery.Callbacks("memory"),
-			jQuery.Callbacks("memory") ]
-	];
+// jQuery 1.6 did not support Callbacks, do not warn there
+if ( jQuery.Callbacks ) {
 
-jQuery.Deferred = function( func ) {
-    var deferred = oldDeferred(),
-		promise = deferred.promise();
+	var oldDeferred = jQuery.Deferred,
+		tuples = [
+			// action, add listener, callbacks, .then handlers, final state
+			[ "resolve", "done", jQuery.Callbacks("once memory"),
+				jQuery.Callbacks("once memory"), "resolved" ],
+			[ "reject", "fail", jQuery.Callbacks("once memory"),
+				jQuery.Callbacks("once memory"), "rejected" ],
+			[ "notify", "progress", jQuery.Callbacks("memory"),
+				jQuery.Callbacks("memory") ]
+		];
 
-	deferred.pipe = promise.pipe = function( /* fnDone, fnFail, fnProgress */ ) {
-		var fns = arguments;
+	jQuery.Deferred = function( func ) {
+		var deferred = oldDeferred(),
+			promise = deferred.promise();
 
-		migrateWarn( "deferred.pipe() is deprecated" );
+		deferred.pipe = promise.pipe = function( /* fnDone, fnFail, fnProgress */ ) {
+			var fns = arguments;
 
-		return jQuery.Deferred(function( newDefer ) {
-			jQuery.each( tuples, function( i, tuple ) {
-				var fn = jQuery.isFunction( fns[ i ] ) && fns[ i ];
-				// deferred.done(function() { bind to newDefer or newDefer.resolve })
-				// deferred.fail(function() { bind to newDefer or newDefer.reject })
-				// deferred.progress(function() { bind to newDefer or newDefer.notify })
-				deferred[ tuple[1] ](function() {
-					var returned = fn && fn.apply( this, arguments );
-					if ( returned && jQuery.isFunction( returned.promise ) ) {
-						returned.promise()
-							.done( newDefer.resolve )
-							.fail( newDefer.reject )
-							.progress( newDefer.notify );
-					} else {
-						newDefer[ tuple[ 0 ] + "With" ](
-							this === promise ? newDefer.promise() : this,
-							fn ? [ returned ] : arguments
-						);
-					}
+			migrateWarn( "deferred.pipe() is deprecated" );
+
+			return jQuery.Deferred(function( newDefer ) {
+				jQuery.each( tuples, function( i, tuple ) {
+					var fn = jQuery.isFunction( fns[ i ] ) && fns[ i ];
+					// deferred.done(function() { bind to newDefer or newDefer.resolve })
+					// deferred.fail(function() { bind to newDefer or newDefer.reject })
+					// deferred.progress(function() { bind to newDefer or newDefer.notify })
+					deferred[ tuple[1] ](function() {
+						var returned = fn && fn.apply( this, arguments );
+						if ( returned && jQuery.isFunction( returned.promise ) ) {
+							returned.promise()
+								.done( newDefer.resolve )
+								.fail( newDefer.reject )
+								.progress( newDefer.notify );
+						} else {
+							newDefer[ tuple[ 0 ] + "With" ](
+								this === promise ? newDefer.promise() : this,
+								fn ? [ returned ] : arguments
+							);
+						}
+					});
 				});
-			});
-			fns = null;
-		}).promise();
+				fns = null;
+			}).promise();
 
+		};
+
+		deferred.isResolved = function() {
+			migrateWarn( "deferred.isResolved is deprecated" );
+			return deferred.state() === "resolved";
+		};
+
+		deferred.isRejected = function() {
+			migrateWarn( "deferred.isRejected is deprecated" );
+			return deferred.state() === "rejected";
+		};
+
+		if ( func ) {
+			func.call( deferred, deferred );
+		}
+
+		return deferred;
 	};
 
-	deferred.isResolved = function() {
-		migrateWarn( "deferred.isResolved is deprecated" );
-		return deferred.state() === "resolved";
-	};
-
-	deferred.isRejected = function() {
-		migrateWarn( "deferred.isRejected is deprecated" );
-		return deferred.state() === "rejected";
-	};
-
-    if ( func ) {
-        func.call( deferred, deferred );
-    }
-
-    return deferred;
-};
+}
