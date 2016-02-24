b645fb3e64925110c181c417cbdee5da1658b9fe
Dave Methvin <dave.methvin@gmail.com>
2016-02-24 08:43:55 -0500

    Build: Make line shorter for JSCS

--- build/release.js
+++ build/release.js
@@ -187,7 +187,8 @@ function pushToRemote( next ) {
 }
 
 function remindAboutCDN( next ) {
-	console.log( chalk.red( "TODO: Update CDN with jquery-migrate." + releaseVersion + " files (min and regular)" ) );
+	console.log( chalk.red( "TODO: Update CDN with jquery-migrate." +
+		releaseVersion + " files (min and regular)" ) );
 	console.log( chalk.red( "  clone codeorigin.jquery.org, git add files, commit, push" ) );
 	next();
 }
