adb28e467a679b552b9bd259392edea3dd050a07
Dave Methvin <dave.methvin@gmail.com>
2016-05-20 14:30:23 -0400

    Build: Don't use commit hooks on version update messages

--- build/release.js
+++ build/release.js
@@ -136,7 +136,7 @@ function checkGitStatus( next ) {
 }
 
 function tagReleaseVersion( next ) {
-	git( [ "commit", "-a", "-m", "Tagging the " + releaseVersion + " release." ], function() {
+	git( [ "commit", "-a", "--no-verify", "-m", "Tagging the " + releaseVersion + " release." ], function() {
 		git( [ "tag", releaseVersion ], next );
 	} );
 }
@@ -183,7 +183,7 @@ function publishToNPM( next ) {
 function setNextVersion( next ) {
 	updateSourceVersion( nextVersion );
 	updatePackageVersion( nextVersion, "master" );
-	git( [ "commit", "-a", "-m", "Updating the source version to " + nextVersion ], next );
+	git( [ "commit", "-a", "--no-verify", "-m", "Updating the source version to " + nextVersion ], next );
 }
 
 function pushToRemote( next ) {
