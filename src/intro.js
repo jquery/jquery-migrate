ae9a49c3567700d9bfdf7e17e678b036f634edf6
Ivan Nikolić <ivan.nikolic@trikoder.net>
2016-07-07 16:15:51 +0200

    Build: add UMD support

--- src/intro.js
+++ src/intro.js
@@ -1,2 +1,14 @@
-(function( jQuery, window ) {
+;(function( factory ) {
+	/* jshint browserify:true */
+	if ( typeof define === "function" && define.amd ) {
+		// AMD. Register as an anonymous module.
+		define( [ "jquery" ], window, factory );
+	} else if ( typeof module === "object" && module.exports ) {
+		// Node/CommonJS
+		module.exports = factory( require( "jquery" ), window );
+	} else {
+		// Browser globals
+		factory( jQuery, window );
+	}
+}(function( jQuery, window ) {
 "use strict";
