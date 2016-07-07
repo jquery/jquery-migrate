6e5f0df53ec330bdffa1a3ad3bbbc1cb4fc2a19b
Ivan Nikolić <ivan.nikolic@trikoder.net>
2016-07-07 16:15:51 +0200

    Build: add UMD support
    Fixes #207
    Closes #206
    
    (cherry picked from commit f92c74340134de7263dc1d7e030eef74a9ca1f2e)

--- src/intro.js
+++ src/intro.js
@@ -1,3 +1,15 @@
-(function( jQuery, window, undefined ) {
+;(function( factory ) {
+	/* jshint browserify:true */
+	if ( typeof define === "function" && define.amd ) {
+		// AMD. Register as an anonymous module.
+		define( [ "jquery" ], window, factory );
+	} else if ( typeof module === "object" && module.exports ) {
+		// Node/CommonJS
+		module.exports = factory( require( "jquery" ), window );
+	} else {
+		// Browser globals
+		factory( jQuery, window );
+	}
+}(function( jQuery, window ) {
 // See http://bugs.jquery.com/ticket/13335
 // "use strict";
