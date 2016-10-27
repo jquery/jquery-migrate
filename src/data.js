43832aebf407aaa029a1c5ccb6326cb2d1c83676
Wesley Mao <wjmao88@gmail.com>
2016-10-27 12:06:37 -0400

    Data: Support object as second argument
    jQuery.data supports passing an object as a second argument and sets data for each entry in that object.
    
    This warns the user about each violating non-camelCase key in the object.
    
    This also adds a type check to avoid a current bug where jQuery.camelCase throws an error because a non-string is passed to it.

--- src/data.js
+++ src/data.js
@@ -2,9 +2,21 @@ var oldData = jQuery.data;
 
 jQuery.data = function( elem, name, value ) {
 	var curData;
+	
+	//name can be an object, and each entry in the object is meant to be set as data
+	if ( name && typeof name === 'object' && arguments.length === 2 ) {
+		for ( var key in name ) {
+			if ( key !== jQuery.camelCase( key ) ){
+				migrateWarn( "jQuery.data() always sets/gets camelCased names: " + key );
+			}
+		}
+		
+		//jQuery.data will convert keys to camelCase, and this is a setter.	
+		return oldData.apply( this, arguments );
+	}
 
 	// If the name is transformed, look for the un-transformed name in the data object
-	if ( name && name !== jQuery.camelCase( name ) ) {
+	if ( name && typeof name === 'string' && name !== jQuery.camelCase( name ) ) {
 		curData = jQuery.hasData( elem ) && oldData.call( this, elem );
 		if ( curData && name in curData ) {
 			migrateWarn( "jQuery.data() always sets/gets camelCased names: " + name );
