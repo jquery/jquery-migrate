73ffd584e1a7bb52109631c2e98dae23d329ed6c
Wesley Mao <wjmao88@gmail.com>
2016-10-27 12:11:48 -0400

    Fix single quote to double quote

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ jQuery.data = function( elem, name, value ) {
 	var curData;
 	
 	//name can be an object, and each entry in the object is meant to be set as data
-	if ( name && typeof name === 'object' && arguments.length === 2 ) {
+	if ( name && typeof name === "object" && arguments.length === 2 ) {
 		for ( var key in name ) {
 			if ( key !== jQuery.camelCase( key ) ){
 				migrateWarn( "jQuery.data() always sets/gets camelCased names: " + key );
@@ -16,7 +16,7 @@ jQuery.data = function( elem, name, value ) {
 	}
 
 	// If the name is transformed, look for the un-transformed name in the data object
-	if ( name && typeof name === 'string' && name !== jQuery.camelCase( name ) ) {
+	if ( name && typeof name === "string" && name !== jQuery.camelCase( name ) ) {
 		curData = jQuery.hasData( elem ) && oldData.call( this, elem );
 		if ( curData && name in curData ) {
 			migrateWarn( "jQuery.data() always sets/gets camelCased names: " + name );
