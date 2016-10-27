b29c94fff7557321169a5ae1c676288a870a09d1
Wesley Mao <wjmao88@gmail.com>
2016-10-27 13:17:54 -0400

    Data: Store non-camel data separately with its non-camel key

--- src/data.js
+++ src/data.js
@@ -5,14 +5,20 @@ jQuery.data = function( elem, name, value ) {
 
 	//Name can be an object, and each entry in the object is meant to be set as data
 	if ( name && typeof name === "object" && arguments.length === 2 ) {
+		curData = jQuery.hasData( elem ) && oldData.call( this, elem );
+		var sameKeys = {};
 		for ( var key in name ) {
 			if ( key !== jQuery.camelCase( key ) ) {
 				migrateWarn( "jQuery.data() always sets/gets camelCased names: " + key );
+				curData[ key ] = name[ key ];
+			} else {
+				sameKeys[ key ] = name[ key ];
 			}
 		}
 
-		//Original jQuery.data will convert keys to camelCase, and this is a setter.
-		return oldData.apply( this, arguments );
+		oldData.call( this, elem, sameKeys );
+
+		return name;
 	}
 
 	// If the name is transformed, look for the un-transformed name in the data object
