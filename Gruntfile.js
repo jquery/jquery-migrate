0f20cc43d8974717f503ffa72777c7bc6cde8589
Dave Methvin <dave.methvin@gmail.com>
2018-03-21 20:32:46 -0400

    squash! Remove custom karma context and debug files

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,8 +86,6 @@ module.exports = function( grunt ) {
 		},
 		karma: {
 			options: {
-				customContextFile: "karma/context.html",
-				customDebugFile: "karma/debug.html",
 				customLaunchers: {
 					ChromeHeadlessNoSandbox: {
 						base: "ChromeHeadless",
