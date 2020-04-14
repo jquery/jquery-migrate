0d0d370360f3396b612d3edb3825058f4630b2d5
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-04-14 11:30:48 +0200

    CSS: Warn and fill .css(name, Number)
    Fixes #296
    Closes #337
    
    Co-authored-by: Dave Methvin <dave.methvin@gmail.com>

--- test/css.js
+++ test/css.js
@@ -41,3 +41,84 @@ QUnit[ ( jQueryVersionSince( "3.4.0" ) && typeof Proxy !== "undefined" ) ? "test
 
 	delete jQuery.cssProps.devoHat;
 } );
+
+QUnit.test( "jQuery.css with numbers", function( assert ) {
+	assert.expect( 5 );
+
+	var whitelist = [
+		"margin",
+		"marginTop",
+		"marginRight",
+		"marginBottom",
+		"marginLeft",
+		"padding",
+		"paddingTop",
+		"paddingRight",
+		"paddingBottom",
+		"paddingLeft",
+		"top",
+		"right",
+		"bottom",
+		"left",
+		"width",
+		"height",
+		"minWidth",
+		"minHeight",
+		"maxWidth",
+		"maxHeight",
+		"border",
+		"borderWidth",
+		"borderTop",
+		"borderTopWidth",
+		"borderRight",
+		"borderRightWidth",
+		"borderBottom",
+		"borderBottomWidth",
+		"borderLeft",
+		"borderLeftWidth"
+	];
+
+	function kebabCase( string ) {
+		return string.replace( /[A-Z]/g, function( match ) {
+			return "-" + match.toLowerCase();
+		} );
+	}
+
+	expectWarning( assert, "Number value direct", function() {
+		jQuery( "<div />" ).css( "line-height", 10 );
+	} );
+
+	expectWarning( assert, "Number in an object", 1, function() {
+		jQuery( "<div />" ).css( {
+			"width": 14,
+			"height": "10px",
+			"line-height": 2
+		} );
+	} );
+
+	expectNoWarning( assert, "String value direct", function() {
+		jQuery( "<div />" ).css( "line-height", "10px" );
+	} );
+
+	expectNoWarning( assert, "String in an object", function() {
+		jQuery( "<div />" ).css( {
+			"width": "14em",
+			"height": "10px",
+			"line-height": "2"
+		} );
+	} );
+
+	expectNoWarning( assert, "Number value (whitelisted props)", function() {
+		whitelist.forEach( function( prop ) {
+			jQuery( "<div />" ).css( prop, 1 );
+			jQuery( "<div />" ).css( kebabCase( prop ), 1 );
+		} );
+	} );
+
+} );
+
+QUnit.test( "jQuery.cssNumber", function( assert ) {
+	assert.expect( 1 );
+
+	assert.ok( jQuery.cssNumber, "jQuery.cssNumber exists" );
+} );
