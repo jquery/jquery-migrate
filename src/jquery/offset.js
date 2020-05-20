dc26258b069af1e04d1db065ee0b27d75a18cc2f
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-05-20 08:37:32 +0200

    Offset: Don't warn against usage on disconnected elements
    The initial decision to not support disconnected elements as input to the
    `.offset()` getter was reversed after feedback from jQuery UI (see gh-2310).
    Since jQuery now ensures disconnected element get `{ left: 0, top: 0 }` from
    this method via unit tests it doesn't make sense to warn against such usage.
    
    This change is the only remaining one in Migrate & UI that's required for UI
    to trigger no Migrate warnings when running its test suite.
    
    Ref jquery/jquery#2310
    Ref jquery/jquery#2396
    Closes gh-359

--- src/jquery/offset.js
+++ src/jquery/offset.js
@@ -2,26 +2,12 @@ import { migrateWarn } from "../main.js";
 
 var oldOffset = jQuery.fn.offset;
 
-function isAttached( elem ) {
-	return jQuery.contains( elem.ownerDocument, elem );
-}
-
 jQuery.fn.offset = function() {
-	var elem = this[ 0 ],
-		bogus = { top: 0, left: 0 };
+	var elem = this[ 0 ];
 
-	if ( !elem || !elem.nodeType ) {
+	if ( elem && ( !elem.nodeType || !elem.getBoundingClientRect ) ) {
 		migrateWarn( "jQuery.fn.offset() requires a valid DOM element" );
 		return arguments.length ? this : undefined;
-	}
-
-	if ( !isAttached( elem ) ) {
-		migrateWarn( "jQuery.fn.offset() requires an element connected to a document" );
-
-		// Only return the bogus value for the getter.
-		if ( !arguments.length ) {
-			return bogus;
-		}
 	}
 
 	return oldOffset.apply( this, arguments );
