39ccd8b7190e1292ececb53993b56c786dbddcee
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2024-10-08 02:10:39 +0200

    Attributes: Restore boolean attribute & false setter treatment from 3.x
    Restore & warn against:
    * boolean attributes set to something different than their lowercase names
    * boolean attributes queried when set to something different than their
      lowercase names
    * non-boolean non-ARIA attributes set to `false`
    
    Fixes gh-504
    Ref jquery/jquery#5452
    Ref jquery/api.jquery.com#1243

--- test/unit/jquery/attributes.js
+++ test/unit/jquery/attributes.js
@@ -1,23 +1,236 @@
-
 QUnit.module( "attributes" );
+
+( function() {
+	function runTests( options ) {
+		var patchEnabled = options.patchEnabled;
+		var stockJq4 = jQueryVersionSince( "4.0.0" ) && !patchEnabled;
+
+		function ifOn( warningsCount ) {
+			return patchEnabled ? warningsCount : 0;
+		}
+
+		QUnit.test( ".attr( boolean attribute ) - patch " +
+				( patchEnabled ? "enabled" : "disabled" ),
+				function( assert ) {
+			assert.expect( 31 );
+
+			if ( !patchEnabled ) {
+				jQuery.migrateDisablePatches( "boolean-attributes" );
+			}
+
+			expectNoWarning( assert, "setting value to null", function() {
+				var $checkbox = jQuery( "<input type='checkbox' checked='checked' />" );
+
+				$checkbox.attr( "checked", null );
+				assert.equal(
+					$checkbox.attr( "checked" ),
+					undefined,
+					"Remove checked by setting to null (verified by .attr)"
+				);
+			} );
+
+			expectWarning( assert, "setting value to true", ifOn( 1 ), function() {
+				var $checkbox = jQuery( "<input type='checkbox' />" );
+
+				$checkbox.prop( "checked", true ).prop( "checked", false ).attr( "checked", true );
+				assert.equal(
+					$checkbox.attr( "checked" ),
+					stockJq4 ? "true" : "checked",
+					"Set checked (verified by .attr)"
+				);
+			} );
+
+			expectWarning( assert, "value-less inline attributes", ifOn( 3 ), function() {
+				var $checkbox = jQuery( "<input checked required autofocus type='checkbox'>" );
+
+				jQuery.each( {
+					checked: "Checked",
+					required: "requiRed",
+					autofocus: "AUTOFOCUS"
+				}, function( lowercased, original ) {
+					try {
+						assert.strictEqual(
+							$checkbox.attr( original ),
+							stockJq4 ? "" : lowercased,
+							"The '" + this +
+								"' attribute getter should return " +
+								( stockJq4 ? "an empty string" : "the lowercased name" )
+						);
+					} catch ( _ ) {
+						assert.ok( false, "The '" + this + "' attribute getter threw" );
+					}
+				} );
+			} );
+
+			expectWarning( assert, "checked: true", ifOn( 1 ), function() {
+				var $checkbox = jQuery( "<input type='checkbox' />" );
+				$checkbox
+					.prop( "checked", true )
+					.prop( "checked", false )
+					.attr( "checked", true );
+				assert.equal(
+					$checkbox.attr( "checked" ),
+					stockJq4 ? "true" : "checked",
+					"Set checked (verified by .attr)"
+				);
+			} );
+			expectNoWarning( assert, "checked: false", function() {
+				var $checkbox = jQuery( "<input type='checkbox' />" );
+				$checkbox
+					.prop( "checked", false )
+					.prop( "checked", true )
+					.attr( "checked", false );
+				assert.equal(
+					$checkbox.attr( "checked" ),
+					undefined,
+					"Remove checked (verified by .attr)"
+				);
+			} );
+
+			expectWarning( assert, "readonly: true", ifOn( 1 ), function() {
+				var $input = jQuery( "<input />" );
+				$input
+					.prop( "readOnly", true )
+					.prop( "readOnly", false )
+					.attr( "readonly", true );
+				assert.equal(
+					$input.attr( "readonly" ),
+					stockJq4 ? "true" : "readonly",
+					"Set readonly (verified by .attr)"
+				);
+			} );
+			expectNoWarning( assert, "readonly: false", function() {
+				var $input = jQuery( "<input />" );
+				$input
+					.prop( "readOnly", false )
+					.prop( "readOnly", true )
+					.attr( "readonly", false );
+				assert.equal(
+					$input.attr( "readonly" ),
+					undefined,
+					"Remove readonly (verified by .attr)"
+				);
+			} );
+
+			expectWarning( assert, "attribute/property interop", ifOn( 2 ), function() {
+				var $checkbox = jQuery( "<input type='checkbox' />" );
+				$checkbox
+					.attr( "checked", true )
+					.attr( "checked", false )
+					.prop( "checked", true );
+				assert.equal( $checkbox[ 0 ].checked, true,
+					"Set checked property (verified by native property)" );
+				assert.equal( $checkbox.prop( "checked" ), true,
+					"Set checked property (verified by .prop)" );
+				assert.equal(
+					$checkbox.attr( "checked" ),
+					undefined,
+					"Setting checked property doesn't affect checked attribute"
+				);
+				$checkbox
+					.attr( "checked", false )
+					.attr( "checked", true )
+					.prop( "checked", false );
+				assert.equal( $checkbox[ 0 ].checked, false,
+					"Clear checked property (verified by native property)" );
+				assert.equal( $checkbox.prop( "checked" ), false,
+					"Clear checked property (verified by .prop)" );
+				assert.equal(
+					$checkbox.attr( "checked" ),
+					stockJq4 ? "true" : "checked",
+					"Clearing checked property doesn't affect checked attribute"
+				);
+			} );
+
+			expectWarning( assert, "HTML5 boolean attributes", ifOn( 2 ), function() {
+				var $input = jQuery( "<input />" );
+				$input.attr( {
+					"autofocus": true,
+					"required": true
+				} );
+				assert.equal(
+					$input.attr( "autofocus" ),
+					stockJq4 ? "true" : "autofocus",
+					"Reading autofocus attribute yields 'autofocus'"
+				);
+				assert.equal(
+					$input.attr( "autofocus", false ).attr( "autofocus" ),
+					undefined,
+					"Setting autofocus to false removes it"
+				);
+				assert.equal(
+					$input.attr( "required" ),
+					stockJq4 ? "true" : "required",
+					"Reading required attribute yields 'required'"
+				);
+				assert.equal(
+					$input.attr( "required", false ).attr( "required" ),
+					undefined,
+					"Setting required attribute to false removes it"
+				);
+			} );
+
+			expectNoWarning( assert, "aria-* attributes", function() {
+				var $input = jQuery( "<input />" );
+
+				$input.attr( "aria-disabled", true );
+				assert.equal(
+					$input.attr( "aria-disabled" ),
+					"true",
+					"Setting aria attributes to true is not affected by boolean settings"
+				);
+
+				$input.attr( "aria-disabled", false );
+				assert.equal(
+					$input.attr( "aria-disabled" ),
+					"false",
+					"Setting aria attributes to false is not affected by boolean settings"
+				);
+			} );
+		} );
+	}
+
+	runTests( { patchEnabled: true } );
+	runTests( { patchEnabled: false } );
+} )();
+
+QUnit.test( ".attr( data-* attribute )", function( assert ) {
+	assert.expect( 6 );
+
+	expectNoWarning( assert, "value: true", function() {
+		var $input = jQuery( "<input />" );
+		$input.attr( "data-something", true );
+		assert.equal( $input.attr( "data-something" ), "true", "Set data attributes" );
+		assert.equal( $input.data( "something" ), true,
+			"Setting data attributes are not affected by boolean settings" );
+	} );
+
+	expectWarning( assert, "value: false", 1, function() {
+		var $input = jQuery( "<input />" );
+		$input.attr( "data-another", false );
+		assert.equal( $input.attr( "data-another" ), "false", "Set data attributes" );
+		assert.equal( $input.data( "another" ), false,
+			"Setting data attributes are not affected by boolean settings" );
+	} );
+} );
 
 QUnit.test( ".removeAttr( boolean attribute )", function( assert ) {
 	assert.expect( 14 );
 
 	expectNoWarning( assert, "non-boolean attr", function() {
 		var $div = jQuery( "<div />" )
-				.attr( "quack", "duck" )
-				.removeAttr( "quack" );
+			.attr( "quack", "duck" )
+			.removeAttr( "quack" );
 
 		assert.equal( $div.attr( "quack" ), null, "non-boolean attribute was removed" );
 		assert.equal( $div.prop( "quack" ), undefined, "property was not set" );
 	} );
 
 	expectWarning( assert, "boolean attr", function() {
-		var $inp = jQuery( "<input type=checkbox/>" )
-				.attr( "checked", "checked" )
-				.prop( "checked", true )
-				.removeAttr( "checked" );
+		var $inp = jQuery( "<input type='checkbox' />" )
+			.attr( "checked", "checked" )
+			.prop( "checked", true )
+			.removeAttr( "checked" );
 
 		assert.equal( $inp.attr( "checked" ), null, "boolean attribute was removed" );
 		assert.equal( $inp.prop( "checked" ), false, "property was changed" );
@@ -25,7 +238,7 @@ QUnit.test( ".removeAttr( boolean attribute )", function( assert ) {
 
 	// One warning per attribute name
 	expectWarning( assert, "multiple boolean attr", 2, function() {
-		jQuery( "<input type=checkbox/>" )
+		jQuery( "<input type='checkbox' />" )
 			.attr( "checked", "checked" )
 			.attr( "readonly", "readonly" )
 			.removeAttr( "checked readonly" );
@@ -41,21 +254,23 @@ QUnit.test( ".removeAttr( boolean attribute )", function( assert ) {
 	} );
 
 	expectNoWarning( assert, "boolean attr when prop false", function() {
-		var $inp = jQuery( "<input type=checkbox/>" )
-				.attr( "checked", "checked" )
-				.prop( "checked", false )
-				.removeAttr( "checked" );
+		var $inp = jQuery( "<input type='checkbox' />" )
+			.attr( "checked", "checked" )
+			.prop( "checked", false )
+			.removeAttr( "checked" );
 
 		assert.equal( $inp.attr( "checked" ), null, "boolean attribute was removed" );
 		assert.equal( $inp.prop( "checked" ), false, "property was not changed" );
 	} );
 
 	expectWarning( assert, "boolean attr when only some props false", 1, function() {
-		var $inp = jQuery( "<input type=checkbox/><input type=checkbox/><input type=checkbox/>" )
-				.attr( "checked", "checked" )
-				.prop( "checked", false )
-				.eq( 1 ).prop( "checked", true ).end()
-				.removeAttr( "checked" );
+		var $inp = jQuery(
+				"<input type='checkbox' /><input type='checkbox' /><input type='checkbox' />"
+			)
+			.attr( "checked", "checked" )
+			.prop( "checked", false )
+			.eq( 1 ).prop( "checked", true ).end()
+			.removeAttr( "checked" );
 
 		assert.equal( $inp.attr( "checked" ), null, "boolean attribute was removed" );
 		assert.equal( $inp.eq( 1 ).prop( "checked" ), false, "property was changed" );
