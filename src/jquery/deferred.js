fbb3a9dfa2b7190f789b47fb592b5581167dfffb
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2024-11-06 09:51:11 +0100

    Core: Don't reimplement deprecated but not removed APIs
    This will save space and avoid potential divergence from Core.
    
    Also, simplify the `deferred.pipe` patch.

--- src/jquery/deferred.js
+++ src/jquery/deferred.js
@@ -8,54 +8,15 @@ import {
 if ( jQuery.Deferred ) {
 
 var unpatchedGetStackHookValue,
-	oldDeferred = jQuery.Deferred,
-	tuples = [
-
-		// Action, add listener, callbacks, .then handlers, final state
-		[ "resolve", "done", jQuery.Callbacks( "once memory" ),
-			jQuery.Callbacks( "once memory" ), "resolved" ],
-		[ "reject", "fail", jQuery.Callbacks( "once memory" ),
-			jQuery.Callbacks( "once memory" ), "rejected" ],
-		[ "notify", "progress", jQuery.Callbacks( "memory" ),
-			jQuery.Callbacks( "memory" ) ]
-	];
+	oldDeferred = jQuery.Deferred;
 
 migratePatchFunc( jQuery, "Deferred", function( func ) {
 	var deferred = oldDeferred(),
 		promise = deferred.promise();
 
-	function newDeferredPipe( /* fnDone, fnFail, fnProgress */ ) {
-		var fns = arguments;
-
-		return jQuery.Deferred( function( newDefer ) {
-			jQuery.each( tuples, function( i, tuple ) {
-				var fn = typeof fns[ i ] === "function" && fns[ i ];
-
-				// Deferred.done(function() { bind to newDefer or newDefer.resolve })
-				// deferred.fail(function() { bind to newDefer or newDefer.reject })
-				// deferred.progress(function() { bind to newDefer or newDefer.notify })
-				deferred[ tuple[ 1 ] ]( function() {
-					var returned = fn && fn.apply( this, arguments );
-					if ( returned && typeof returned.promise === "function" ) {
-						returned.promise()
-							.done( newDefer.resolve )
-							.fail( newDefer.reject )
-							.progress( newDefer.notify );
-					} else {
-						newDefer[ tuple[ 0 ] + "With" ](
-							this === promise ? newDefer.promise() : this,
-							fn ? [ returned ] : arguments
-						);
-					}
-				} );
-			} );
-			fns = null;
-		} ).promise();
-	}
-
-	migratePatchAndWarnFunc( deferred, "pipe", newDeferredPipe, "deferred-pipe",
+	migratePatchAndWarnFunc( deferred, "pipe", deferred.pipe, "deferred-pipe",
 		"deferred.pipe() is deprecated" );
-	migratePatchAndWarnFunc( promise, "pipe", newDeferredPipe, "deferred-pipe",
+	migratePatchAndWarnFunc( promise, "pipe", promise.pipe, "deferred-pipe",
 		"deferred.pipe() is deprecated" );
 
 	if ( func ) {
--- src/jquery/event.js
+++ src/jquery/event.js
@@ -26,29 +26,18 @@ jQuery.each( ( "blur focus focusin focusout resize scroll click dblclick " +
 function( _i, name ) {
 
 	// Handle event binding
-	migratePatchAndWarnFunc( jQuery.fn, name, function( data, fn ) {
-		return arguments.length > 0 ?
-			this.on( name, null, data, fn ) :
-			this.trigger( name );
-	},
-	"shorthand-deprecated-v3",
-	"jQuery.fn." + name + "() event shorthand is deprecated" );
+	migratePatchAndWarnFunc( jQuery.fn, name, jQuery.fn[ name ], "shorthand-deprecated-v3",
+		"jQuery.fn." + name + "() event shorthand is deprecated" );
 } );
 
-migratePatchAndWarnFunc( jQuery.fn, "bind", function( types, data, fn ) {
-	return this.on( types, null, data, fn );
-}, "pre-on-methods", "jQuery.fn.bind() is deprecated" );
-migratePatchAndWarnFunc( jQuery.fn, "unbind", function( types, fn ) {
-	return this.off( types, null, fn );
-}, "pre-on-methods", "jQuery.fn.unbind() is deprecated" );
-migratePatchAndWarnFunc( jQuery.fn, "delegate", function( selector, types, data, fn ) {
-	return this.on( types, selector, data, fn );
-}, "pre-on-methods", "jQuery.fn.delegate() is deprecated" );
-migratePatchAndWarnFunc( jQuery.fn, "undelegate", function( selector, types, fn ) {
-	return arguments.length === 1 ?
-		this.off( selector, "**" ) :
-		this.off( types, selector || "**", fn );
-}, "pre-on-methods", "jQuery.fn.undelegate() is deprecated" );
-migratePatchAndWarnFunc( jQuery.fn, "hover", function( fnOver, fnOut ) {
-	return this.on( "mouseenter", fnOver ).on( "mouseleave", fnOut || fnOver );
-}, "hover", "jQuery.fn.hover() is deprecated" );
+migratePatchAndWarnFunc( jQuery.fn, "bind", jQuery.fn.bind,
+	"pre-on-methods", "jQuery.fn.bind() is deprecated" );
+migratePatchAndWarnFunc( jQuery.fn, "unbind", jQuery.fn.unbind,
+	"pre-on-methods", "jQuery.fn.unbind() is deprecated" );
+migratePatchAndWarnFunc( jQuery.fn, "delegate", jQuery.fn.delegate,
+	"pre-on-methods", "jQuery.fn.delegate() is deprecated" );
+migratePatchAndWarnFunc( jQuery.fn, "undelegate", jQuery.fn.undelegate,
+	"pre-on-methods", "jQuery.fn.undelegate() is deprecated" );
+
+migratePatchAndWarnFunc( jQuery.fn, "hover", jQuery.fn.hover,
+	"hover", "jQuery.fn.hover() is deprecated" );
