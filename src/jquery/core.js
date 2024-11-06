fa58d2b14739cc186fc95ba8206e16d881a12853
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2024-11-06 09:51:51 +0100

    Core: Don't reimplement deprecated but not removed APIs
    This will save space and avoid potential divergence from Core.
    
    To minimize risk, this only handles APIs still present in jQuery 4.x.

--- src/jquery/core.js
+++ src/jquery/core.js
@@ -2,9 +2,7 @@ import { jQueryVersionSince } from "../compareVersions.js";
 import { migratePatchAndWarnFunc } from "../main.js";
 import "../disablePatches.js";
 
-var arr = [],
-	slice = arr.slice,
-	class2type = {},
+var class2type = {},
 
 	// Require that the "whitespace run" starts from a non-whitespace
 	// to avoid O(N^2) behavior when the engine would try matching "\s+$" at each space position.
@@ -116,34 +114,7 @@ if ( jQueryVersionSince( "3.3.0" ) ) {
 	// arguments.
 	// jQuery.proxy is deprecated to promote standards (specifically Function#bind)
 	// However, it is not slated for removal any time soon
-	migratePatchAndWarnFunc( jQuery, "proxy",
-		function( fn, context ) {
-			var tmp, args, proxy;
-
-			if ( typeof context === "string" ) {
-				tmp = fn[ context ];
-				context = fn;
-				fn = tmp;
-			}
-
-			// Quick check to determine if target is callable, in the spec
-			// this throws a TypeError, but we will just return undefined.
-			if ( !isFunction( fn ) ) {
-				return undefined;
-			}
-
-			// Simulated bind
-			args = slice.call( arguments, 2 );
-			proxy = function() {
-				return fn.apply( context || this, args.concat( slice.call( arguments ) ) );
-			};
-
-			// Set the guid of unique handler to the same of original handler, so it can be removed
-			proxy.guid = fn.guid = fn.guid || jQuery.guid++;
-
-			return proxy;
-		}, "proxy",
-		"jQuery.proxy() is deprecated"
-	);
+	migratePatchAndWarnFunc( jQuery, "proxy", jQuery.proxy,
+		"proxy", "DEPRECATED: jQuery.proxy()" );
 
 }
