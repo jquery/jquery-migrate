868e11d23c06ea5fa60326701b2de27951442949
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2025-03-03 18:16:55 +0100

    Data: Patch & warn about props from Object.prototype in data objects
    Closes gh-561
    Ref gh-559

--- test/data/testinit.js
+++ test/data/testinit.js
@@ -62,6 +62,7 @@
 				"unit/jquery/ajax.js",
 				"unit/jquery/attributes.js",
 				"unit/jquery/css.js",
+				"unit/jquery/data.js",
 				"unit/jquery/deferred.js",
 				"unit/jquery/effects.js",
 				"unit/jquery/event.js",
